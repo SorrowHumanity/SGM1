50b1b80dce90a1c0a69de920b5ce2e08b23bcd98
Stoyan <wpckishere@gmail.com>
2018-04-24 08:36:14 +0200

    Made camera follow multiplayer

---
+++ CyberPunk v2/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerManager : NetworkBehaviour {
+
+    [SerializeField]
+    Behaviour[] components;
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < components.Length; i++) {
+                components[i].enabled = false;
+            }
+        }
+        else
+        {
+            if(sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if(sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(false);
+        }
+    }
+
+}
