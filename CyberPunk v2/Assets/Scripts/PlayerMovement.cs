5e52e286715c29dfe530c0a0701a17bd761c02c6
Stoyan <wpckishere@gmail.com>
2018-04-29 20:56:13 +0200

    New cam + movement

--- CyberPunk v2/Assets/Scripts/PlayerMovement.cs
+++ CyberPunk v2/Assets/Scripts/PlayerMovement.cs
@@ -10,8 +10,10 @@ public class PlayerMovement : MonoBehaviour {
     public CharacterController controller;
     private Vector3 moveDirection;
     public float gravityScale;
-
+    public float rotateSpeed;
+    public Transform pivot;
     public Animator anim;
+    public GameObject playerModel;
 
 	// Use this for initialization
 	void Start () {
@@ -21,12 +23,16 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y , Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
 
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
 
         if (controller.isGrounded)
         {
             moveDirection.y = 0f;
+
             if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
@@ -36,6 +42,14 @@ public class PlayerMovement : MonoBehaviour {
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
 
         controller.Move(moveDirection * Time.deltaTime);
+
+        if(Input.GetAxis("Horizontal") !=0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
 
         anim.SetBool("isGrounded", controller.isGrounded);
 
